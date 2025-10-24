169e5b308ef77da681303d4eebb4c7373b182100
Schugu <leo.schugu@gmail.com>
2025-10-24 12:23:03 -0300

    add: export de función

--- src/stock/categories/functions.h
+++ src/stock/categories/functions.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_CATEGORIES_H
 #define FUNCTIONS_CATEGORIES_H
 
+int categoriaExiste(const char*);
 void leerCategorias();
 void crearCategoria();
 void filtrarCategoriaPorNombre();
